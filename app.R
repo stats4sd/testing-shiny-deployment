1d042858e05d033858d080b1cf2b03fc8cf98927
Dave Mills <dave-mills@users.noreply.github.com>
2023-09-14 12:05:34 +0100

    Update app.R

--- app.R
+++ app.R
@@ -23,7 +23,7 @@ neg<-function(x){-1*x}
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Asking Better Questions"),
+   titlePanel("Asking Better Questions, Edited For Deployment Testing"),
    
    
    # Sidebar with a slider input for number of bins 
